e3b818c3de6b3a817d16a4e163e9945e6e129526
h2a64zol <138403118+h2a64zol@users.noreply.github.com>
2024-01-02 14:57:27 +0200

    Add files via upload

--- assets/data.ts
+++ assets/data.ts
@@ -16,5 +16,23 @@ export var prod = [
       "name": "Інфо-Бухгалтер 8.6 мережева версія (до 10 робочих місць) для Windows",
       "price": 10990,
       "image": "assets/images/splash256.png"
+    },
+    {
+        "id": 4,
+      "name": "Інфо-Бухгалтер 8.6 локальна версія для Windows",
+      "price": 6890,
+      "image": "assets/images/splash256.png"
+    },
+    {
+        "id": 5,
+      "name": "Інфо-Бухгалтер 8.6 мережева версія (до 5 робочих місць) для Windows",
+      "price": 9290,
+      "image": "assets/images/splash256.png"
+    },
+    {
+        "id": 6,
+      "name": "Інфо-Бухгалтер 8.6 мережева версія (до 10 робочих місць) для Windows",
+      "price": 10990,
+      "image": "assets/images/splash256.png"
     }
 ]
